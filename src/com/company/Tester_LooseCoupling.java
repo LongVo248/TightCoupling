4506078f8312265980f9dbf43c72356ff6748b0b
LongVo248 <vohoanglongfx@gmail.com>
2021-12-14 13:59:08 +0700

    Initial commit

---
+++ src/com/company/Tester_LooseCoupling.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Tester_LooseCoupling {
+    public static void main(String args[]) throws IOException {
+        Show b1 = new B1();
+        Show c = new C();
+
+        A1 a = new A1(b1);
+        //a.display() will print A and B
+        a.display();
+
+        A1 a1 = new A1(c);
+        //a.display() will print A and C
+        a1.display();
+    }
+}
+interface Show {
+    public void display();
+}
+
+class A1 {
+    Show s;
+    public A1(Show s) {
+        //s is loosely coupled to A
+        this.s = s;
+    }
+
+    public void display() {
+        System.out.println("A");
+        s.display();
+    }
+}
+
+class B1 implements Show {
+    public B1(){}
+    public void display() {
+        System.out.println("B");
+    }
+}
+
+class C implements Show {
+    public C(){}
+    public void display() {
+        System.out.println("C");
+    }
+}
---
+++ src/com/company/Tester_TightCoupling.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Tester_TightCoupling {
+    public static void main(String[] args) {
+        A a = new A();
+        a.display();
+    }
+}
+
+class A{
+    B b;
+    public A(){
+        b= new B();
+    }
+    public void display(){
+        System.out.println("A");
+        b.display();
+    }
+}
+
+class B{
+    public B(){}
+    public void display(){
+        System.out.println("B");
+    }
+}
